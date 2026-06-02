52e3a676ea7ac3eb999740c3ad250da53f33a557
alejandro871 <alejandro.barahona@correounivalle,edu,co>
2026-06-02 18:00:13 -0500

    ultimo commit mp2

--- vista/Main.java
+++ vista/Main.java
@@ -5,7 +5,7 @@ import javax.swing.SwingUtilities;
 public class Main {
 
     public static void main(String[] args) {
-        // toda la UI de Swing debe correr en el Event Dispatch Thread (EDT)
+
         SwingUtilities.invokeLater(() -> {
             PantallaInicio pantalla = new PantallaInicio();
             pantalla.setVisible(true);
